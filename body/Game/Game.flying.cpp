cc272ee05b5f542b73cb24aa5d5d2837e9f303de
lexyDiv <aledab@mail.ru>
2024-07-03 12:37:46 +0300

    before splice rapid

--- body/Game/Game.flying.cpp
+++ body/Game/Game.flying.cpp
@@ -77,6 +77,7 @@ void Game::flyingMove()
                 if (roll != nullptr)
                 {
                     this->pause = true;
+                    roll = nullptr;
                 }
                 else
                 {
